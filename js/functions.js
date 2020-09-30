f450bb9677192c2eab6aec5875778c984e99e8de
ilonabann <70884407+ilonabann@users.noreply.github.com>
2020-09-30 14:20:25 +0300

    Basic structure

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+import suma from './suma.js';
+import atimtis from './atimtis.js'; 
+
+import posts from './data.js';
+
+
+import sayHi from './sayHi.js';
+
+console.log(posts);
+
+console.log(sayHi('Bagira'));
+
+console.log(suma(4, 8));
+console.log(atimtis(14, 8));
+
+console.log(sayHi('Rex'));
